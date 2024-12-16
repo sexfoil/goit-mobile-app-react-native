f10e29716e845618716d262a9f2a586e9bb65560
Slava Poliakov <78slavapolyakov@gmail.com>
2024-12-16 21:39:52 +0200

    Add redux files

---
+++ MobileAppHW/redux/reducers/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInfo: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUserInfo(state, action) {
+      state.userInfo = action.payload;
+    },
+    clearUserInfo(state) {
+      state.userInfo = null;
+    },
+  },
+});
+
+export const { setUserInfo, clearUserInfo } = userSlice.actions;
+
+export default userSlice.reducer;
